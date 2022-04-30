4b4f70447b62492c1ef1f0239741f6ae50887158
Bealia <tsadok.nguessan@smile.ci>
2022-04-30 16:51:47 +0000

    first commit

---
+++ src/views/HomePage.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <div class="container">
+        <div class="w-75 mx-auto my-5 text-center">
+            
+            <div class="card p-2 bg-success " >
+                <h1>Bienvenue Vous etes sur l'accueil My Agri Bank CI</h1>
+            </div>
+        </div>
+        </div>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+
+        }
+    },
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
